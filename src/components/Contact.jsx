99d458bccc79a4ae92c5e5c08c2aa0d78979fe23
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-10-11 17:27:40 +0500

    completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div
-      className="max-w-[1400px] mx-auto bg-background sm:py-20 p-5"
-      id="contact"
-    >
+    <div className="max-w-[1400px] mx-auto bg-background sm:py-20 p-5" id="contact">
       <div className="text-center mt-8">
         <h2 className="text-4xl font-bold leading-tight text-primary">
           Contact Me
@@ -29,11 +26,11 @@ const Contact = () => {
                         name="name"
                         id="name"
                         placeholder="Your Name"
-                        className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
+                        className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 bg-white border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
                       />
                     </div>
                   </div>
-                  
+
                   <div>
                     <div className="mt-2.5 relative">
                       <input
@@ -41,19 +38,21 @@ const Contact = () => {
                         name="email"
                         id="email"
                         placeholder="Your Email"
-                        className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
+                        className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 bg-white border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
                       />
                     </div>
                   </div>
+
                   <div className="col-span-full">
                     <textarea
                       name="message"
                       id="message"
                       placeholder="Your Message"
                       rows="4"
-                      className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
+                      className="w-full px-4 py-4 text-gray-700 placeholder-gray-500 bg-white border-2 border-gray-400 rounded-md focus:outline-none focus:border-accent transition-colors duration-300"
                     ></textarea>
                   </div>
+
                   <div className="col-span-full">
                     <button
                       type="submit"
